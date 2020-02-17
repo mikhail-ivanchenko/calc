0fffa35f4619868720b0e0945711126c3bfc9b79
Савонова <@STUDENT17-4>
2020-02-17 10:14:13 +0300

    Кнопка умножения

--- Calc/Form1.cs
+++ Calc/Form1.cs
@@ -1,11 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Calc
@@ -16,5 +9,18 @@ namespace Calc
         {
             InitializeComponent();
         }
+
+
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            float A = float.Parse(textBox1.Text);
+            float B = float.Parse(textBox2.Text);
+            float result;
+            result = A * B;
+            textBox3.Text = result.ToString();
+        }
+
+
     }
 }
