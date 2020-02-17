e0a52df0cecd2f97ed3a09c6c54c46b7ab76222b
Строилов <@STUDENT17-3>
2020-02-17 09:56:00 +0300

    Действие с вычислением

--- Calc/Form1.cs
+++ Calc/Form1.cs
@@ -16,5 +16,14 @@ namespace Calc
         {
             InitializeComponent();
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+                                        float c;
+            float a = float.Parse(textBox1.Text);
+            float b = float.Parse(textBox2.Text);
+            c = a - b;
+            textBox3.Text =  c.ToString();
+        }
     }
 }
